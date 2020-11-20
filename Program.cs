e71de1db471fbc2877f9b8390d9feccc303ea5b9
Paul-Eric Lange <lange.paul.eric@gmail.com>
2020-11-20 12:20:28 +0100

    Inital repo setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LOTM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
