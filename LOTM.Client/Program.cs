17d80f96ee2c21e6a9479cbbbd18183997615d16
Paul-Eric Lange <lange.paul.eric@gmail.com>
2020-11-20 12:38:05 +0100

    Updated structure to Client, Server and Shared

--- LOTM.Client/Program.cs
+++ LOTM.Client/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace LOTM
+namespace LOTM.Client
 {
     class Program
     {
---
+++ LOTM.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LOTM.Shared
+{
+    public class Class1
+    {
+    }
+}
