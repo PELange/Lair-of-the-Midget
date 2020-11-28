e995e2eadfecde8dfa909d0efc0dd167ac99a0a3
Paul-Eric Lange <lange.paul.eric@gmail.com>
2020-11-28 20:45:13 +0100

    rotation test

--- LOTM.Client/Engine/Graphics/Renderer2D.cs
+++ LOTM.Client/Engine/Graphics/Renderer2D.cs
@@ -120,57 +120,71 @@ namespace LOTM.Client.Engine.Graphics
         {
             var verticies = new List<Vertex>
             {
-                new Vertex(new Vector2(  0,  0), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f), 0),
-                new Vertex(new Vector2(  0, 50), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 1.0f), 0),
-                new Vertex(new Vector2( 50,  0), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 0.0f), 0),
-                new Vertex(new Vector2( 50, 50), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f), 0),
+                new Vertex(new Vector2( 200, 200), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f), 0),
+                new Vertex(new Vector2( 200, 250), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 1.0f), 0),
+                new Vertex(new Vector2( 250, 200), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 0.0f), 0),
+                new Vertex(new Vector2( 250, 250), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f), 0),
 
-                new Vertex(new Vector2(100,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 0.0f), 1),
-                new Vertex(new Vector2(100, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 1.0f), 1),
-                new Vertex(new Vector2(150,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 0.0f), 1),
-                new Vertex(new Vector2(150, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 1.0f), 1),
+                //new Vertex(new Vector2(  0,  0), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f), 0),
+                //new Vertex(new Vector2(  0, 50), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(0.0f, 1.0f), 0),
+                //new Vertex(new Vector2( 50,  0), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 0.0f), 0),
+                //new Vertex(new Vector2( 50, 50), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f), 0),
 
-                new Vertex(new Vector2(200,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 0.0f), 1),
-                new Vertex(new Vector2(200, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 1.0f), 1),
-                new Vertex(new Vector2(250,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 0.0f), 1),
-                new Vertex(new Vector2(250, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 1.0f), 1),
+                //new Vertex(new Vector2(100,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 0.0f), 1),
+                //new Vertex(new Vector2(100, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 1.0f), 1),
+                //new Vertex(new Vector2(150,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 0.0f), 1),
+                //new Vertex(new Vector2(150, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 1.0f), 1),
+
+                //new Vertex(new Vector2(200,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 0.0f), 1),
+                //new Vertex(new Vector2(200, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.0f, 1.0f), 1),
+                //new Vertex(new Vector2(250,  0), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 0.0f), 1),
+                //new Vertex(new Vector2(250, 50), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), new Vector2(1.0f, 1.0f), 1),
             };
 
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    var data = verticies[i];
+            for (int i = 0; i < 4; i++)
+            {
+                var data = verticies[i];
 
-            //    //var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 0))
-            //    //    * glm.rotate(mat4.identity(), glm.radians(45.0f), new vec3(0, 0, 1))
-            //    //    * glm.scale(mat4.identity(), new vec3(1.5f, 1.5f, 1));
-            //    ////transform = glm.translate(transform, new vec3(-data.Position[0], -data.Position[1], 0));
+                //var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 0))
+                //        * glm.rotate(mat4.identity(), glm.radians(45.0f), new vec3(0, 0, 1))
+                //        * glm.scale(mat4.identity(), new vec3(1.5f, 1.5f, 1));
+                ////transform = glm.translate(transform, new vec3(-data.Position[0], -data.Position[1], 0));
 
-            //    //var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 1))
-            //    //    * glm.rotate(mat4.identity(), glm.radians(45.0f), new vec3(0, 0, 1))
-            //    //    * glm.scale(mat4.identity(), new vec3(1.0f, 1.0f, 1));
+                ////var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 1))
+                ////    * glm.rotate(mat4.identity(), glm.radians(45.0f), new vec3(0, 0, 1))
+                ////    * glm.scale(mat4.identity(), new vec3(1.0f, 1.0f, 1));
 
-            //    //float size = 1;
-            //    //float rotate = 45.0f;
-            //    //var transform = new mat4(1.0f);
-            //    //transform = glm.translate(new mat4(1.0f), new vec3(data.Position[0], data.Position[1], 0.0f));
-            //    //transform = glm.translate(transform, new vec3(0.5f * size, 0.5f * size, 0.0f));
-            //    //transform = glm.rotate(transform, glm.radians(rotate), new vec3(0.0f, 0.0f, 1.0f));
-            //    //transform = glm.translate(transform, new vec3(-0.5f * size, -0.5f * size, 0.0f));
-            //    //transform = glm.scale(transform, new vec3(size, size, 1.0f));
+                ////float size = 1;
+                ////float rotate = 45.0f;
+                ////var transform = new mat4(1.0f);
+                ////transform = glm.translate(new mat4(1.0f), new vec3(data.Position[0], data.Position[1], 0.0f));
+                ////transform = glm.translate(transform, new vec3(0.5f * size, 0.5f * size, 0.0f));
+                ////transform = glm.rotate(transform, glm.radians(rotate), new vec3(0.0f, 0.0f, 1.0f));
+                ////transform = glm.translate(transform, new vec3(-0.5f * size, -0.5f * size, 0.0f));
+                ////transform = glm.scale(transform, new vec3(size, size, 1.0f));
 
-            //    ////var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 0.0f));
-            //    ////transform = glm.rotate(transform, glm.radians(45), new vec3(0.0f, 0.0f, 1.0f));
-            //    ////transform = glm.translate(transform, new vec3(-data.Position[0], -data.Position[1], 0));
+                //////var transform = glm.translate(mat4.identity(), new vec3(data.Position[0], data.Position[1], 0.0f));
+                //////transform = glm.rotate(transform, glm.radians(45), new vec3(0.0f, 0.0f, 1.0f));
+                //////transform = glm.translate(transform, new vec3(-data.Position[0], -data.Position[1], 0));
 
-            //    //var transformedPos = transform * qvp;
-            //    var transformedPos = transform * new vec4(data.Position[0], data.Position[1], 1, 1);
-            //    //var transformedPos = transform * new vec4(10, 10, 1, 1);
-            //    data.Position[0] = transformedPos.x;
-            //    data.Position[1] = transformedPos.y;
+                ////var transformedPos = transform * qvp;
+                //var transformedPos = transform * new vec4(1, 1, 1, 1);
+                ////var transformedPos = transform * new vec4(10, 10, 1, 1);
+                //data.Position[0] = transformedPos.x;
+                //data.Position[1] = transformedPos.y;
 
-            //    verticies.RemoveAt(i);
-            //    verticies.Insert(i, data);
-            //}
+                var v4In = new vec4(data.Position[0], data.Position[1], 0.0f, 1.0f);
+                var v4RotCenter = new vec4(225, 225, 0.0f, 1.0f);
+                var v3RotAxis = new vec3(0.0f, 0.0f, 1.0f);
+                var matRot = glm.rotate(glm.radians(45.0f), v3RotAxis);
+                var result = rotateAround(v4In, v4RotCenter, matRot);
+
+                data.Position[0] = result.x;
+                data.Position[1] = result.y;
+
+                verticies.RemoveAt(i);
+                verticies.Insert(i, data);
+            }
 
             //Set dynamic vertex buffer
             glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
@@ -195,6 +209,21 @@ namespace LOTM.Client.Engine.Graphics
             glBindVertexArray(0);
 
             Shader.Unbind();
+        }
+
+        vec4 rotateAround(vec4 aPointToRotate, vec4 aRotationCenter, mat4 aRotationMatrix)
+        {
+            var translate = glm.translate(new mat4(1.0f), new vec3(aRotationCenter.x, aRotationCenter.y, aRotationCenter.z));
+            var invTranslate = glm.inverse(translate);
+
+            // The idea:
+            // 1) Translate the object to the center
+            // 2) Make the rotation
+            // 3) Translate the object back to its original location
+
+            var transform = translate * aRotationMatrix * invTranslate;
+
+            return transform * aPointToRotate;
         }
     }
 }
