635602375337add6b198a45deae0d8e529605ef9
Paul-Eric Lange <lange.paul.eric@gmail.com>
2020-12-19 11:47:16 +0100

    Fixed warning

--- LOTM.Shared/Engine/Math/Vector4.cs
+++ LOTM.Shared/Engine/Math/Vector4.cs
@@ -3,7 +3,7 @@
     public class Vector4 : Vector3
     {
         public static new Vector4 ZERO => new Vector4(0, 0, 0, 0);
-        public static new Vector4 ONE => new Vector4(1, 1, 1, 1);
+        public static Vector4 ONE => new Vector4(1, 1, 1, 1);
 
         public double W { get; set; }
 
